4e5d40890abebcdb96949efcc71936d6c3bacee1
Richard Jones <contact@richardjones.io>
2024-09-08 11:25:56 +0100

    Add enforce no empty lines property

--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/codestyle/StreamOperationsRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/codestyle/StreamOperationsRule.java
@@ -16,11 +16,24 @@ import net.sourceforge.pmd.lang.java.types.JTypeMirror;
 import net.sourceforge.pmd.lang.java.types.OverloadSelectionResult;
 import net.sourceforge.pmd.lang.java.types.TypeTestUtil;
 import net.sourceforge.pmd.lang.rule.RuleTargetSelector;
+import net.sourceforge.pmd.properties.PropertyDescriptor;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 import java.util.stream.BaseStream;
 
+import static net.sourceforge.pmd.properties.PropertyFactory.booleanProperty;
+
 public class StreamOperationsRule extends AbstractJavaRule {
+
+    private static final PropertyDescriptor<Boolean> ENFORCE_NO_EMPTY_LINES_DESCRIPTOR =
+            booleanProperty("enforceNoEmptyLines")
+                    .defaultValue(false)
+                    .desc("Ignore methods that have annotations (except @Override)")
+                    .build();
+
+    public StreamOperationsRule() {
+        definePropertyDescriptor(ENFORCE_NO_EMPTY_LINES_DESCRIPTOR);
+    }
 
     @Override
     protected @NonNull RuleTargetSelector buildTargetSelector() {
@@ -32,7 +45,7 @@ public class StreamOperationsRule extends AbstractJavaRule {
         ASTExpression expr = node.getExpr();
         if (expr instanceof ASTMethodCall) {
             ASTMethodCall call = (ASTMethodCall) expr;
-            process(node, data, call);
+            processMethodCall(node, data, call);
         }
 
         return data;
@@ -42,13 +55,13 @@ public class StreamOperationsRule extends AbstractJavaRule {
     public Object visit(ASTLocalVariableDeclaration node, Object data) {
         for (ASTVariableDeclarator variable : node.children(ASTVariableDeclarator.class)) {
             for (ASTMethodCall call : variable.children(ASTMethodCall.class)) {
-                process(node, data, call);
+                processMethodCall(node, data, call);
             }
         }
         return data;
     }
 
-    private void process(JavaNode node, Object data, ASTMethodCall call) {
+    private void processMethodCall(JavaNode node, Object data, ASTMethodCall call) {
         if (isStreamAndUsesStreamMethod(call)) {
             asCtx(data).addViolation(node);
         }
@@ -63,6 +76,7 @@ public class StreamOperationsRule extends AbstractJavaRule {
     // TODO source operation on new lines property - enabled, disabled, ignored
     // TODO empty lines property - boolean
     private boolean checkLines(JavaNode node) {
+
         if (node == null || node.getNumChildren() == 0) {
             return false;
         }
@@ -81,8 +95,17 @@ public class StreamOperationsRule extends AbstractJavaRule {
         return false;
     }
 
-    private static boolean checkSiblings(JavaNode previousSibling, JavaNode currentSibling) {
-        return previousSibling != null && currentSibling.getBeginLine() <= previousSibling.getEndLine();
+    private boolean checkSiblings(JavaNode previousSibling, JavaNode currentSibling) {
+        if (previousSibling == null) {
+            return false;
+        }
+
+        boolean shouldEnforceNoEmptyLines = getProperty(ENFORCE_NO_EMPTY_LINES_DESCRIPTOR);
+        if (shouldEnforceNoEmptyLines) {
+            return currentSibling.getBeginLine() != previousSibling.getEndLine() + 1;
+        } else {
+            return currentSibling.getBeginLine() <= previousSibling.getEndLine();
+        }
     }
 
     private boolean isStreamReturnType(JavaNode node) {
@@ -101,8 +124,6 @@ public class StreamOperationsRule extends AbstractJavaRule {
         JMethodSig methodType = result.getMethodType();
         JTypeMirror returnType = methodType.getReturnType();
         return TypeTestUtil.isA(BaseStream.class, returnType);
-//        JExecutableSymbol symbol = methodType.getSymbol();
-//        return TypeTestUtil.isExactlyA(BaseStream.class, symbol.getEnclosingClass());
     }
 
 }
